b6167af4b7b37ad9764e2683be4b5a3abed599d7
Borja418 <borjapeiroten@gmail.com>
2024-02-23 12:54:11 +0100

    Eliminar mala detección y cambiar URI

--- calibracion.py
+++ calibracion.py
@@ -7,22 +7,23 @@ import os
 
 def calibrar():
 
-    imgs_name = [f for f in os.listdir("img_calib") if f.endswith('.png')]
+    imgs_name = [f for f in os.listdir("Real_Imgs") if f.endswith('.jpg')]
     
     for img_name in imgs_name:
 
-        img = cv2.imread("img_calib/"+img_name)
-
+        img = cv2.imread("Real_Imgs/"+img_name)
+        print(img_name)
         # Comprobamos que la imagen se ha podido leer
         if img is None:
             print('Error al cargar la imagen')
             quit()
 
+        img = cv2.resize(img, (1200,675))
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
         ret, corners = cv2.findChessboardCorners(img_gray, (9,6), None)
-        print(corners)
+        
         if ret:
 
             img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
