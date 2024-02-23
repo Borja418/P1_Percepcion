efab5585c2a34bd98d517d51e26ba8a7a4905880
Borja418 <borjapeiroten@gmail.com>
2024-02-23 15:53:34 +0100

    Calibración de camara

--- calibracion.py
+++ calibracion.py
@@ -5,14 +5,24 @@ import cv2
 import numpy as np
 import os
 
+LONGITUD = 2
+
+
 def calibrar():
 
+    objpoints = np.zeros((9*6,3),np.float32)
+    objpoints[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+    objpoints = objpoints*LONGITUD
+
+    objpoints_array = []
+    imgpoints_array = []
     imgs_name = [f for f in os.listdir("Real_Imgs") if f.endswith('.jpg')]
     
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
     for img_name in imgs_name:
 
         img = cv2.imread("Real_Imgs/"+img_name)
-        print(img_name)
         # Comprobamos que la imagen se ha podido leer
         if img is None:
             print('Error al cargar la imagen')
@@ -25,13 +35,21 @@ def calibrar():
         ret, corners = cv2.findChessboardCorners(img_gray, (9,6), None)
         
         if ret:
+            objpoints_array.append(objpoints)
 
-            img = cv2.drawChessboardCorners(img, (9,6), corners, ret)
+            corners2 = cv2.cornerSubPix(img_gray, corners, (11,11), (-1,-1), criteria)
+            imgpoints_array.append(corners2)
 
-            cv2.imshow("Esquinas",img)
-            cv2.waitKey(0)
+            img = cv2.drawChessboardCorners(img, (9,6), corners2, ret)
+
+            """ cv2.imshow("Esquinas",img)
+            cv2.waitKey(0) """
+    
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints_array, imgpoints_array, img_gray.shape[::-1], None, None)
 
     print(f"Numero de imagenes para la calibracion: {len(imgs_name)}")
 
+    return mtx, dist, rvecs, tvecs
+
 if __name__ == '__main__':
-    calibrar()
+    mtx, dist, rvecs, tvecs = calibrar()
