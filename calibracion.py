6fb1d6e1128b5574266635699ebd9c33c582d89b
Borja418 <borjapeiroten@gmail.com>
2024-03-13 22:01:01 +0100

    PruebaMtxVideoInicial

--- calibracion.py
+++ calibracion.py
@@ -22,19 +22,19 @@ def calibrar():
 
     objpoints_array = []
     imgpoints_array = []
-    imgs_name = [f for f in os.listdir("MovilBorja") if f.endswith('.jpg')]
+    imgs_name = [f for f in os.listdir("Real_Imgs") if f.endswith('.jpg')]
     
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
     i = 0
     for img_name in imgs_name:
         i+=1
-        img = cv2.imread("MovilBorja/"+img_name)
+        img = cv2.imread("Real_Imgs/"+img_name)
         # Comprobamos que la imagen se ha podido leer
         if img is None:
             print('Error al cargar la imagen')
             quit()
 
-        img = cv2.resize(img, ESCALA_BORJA)
+        img = cv2.resize(img, ESCALA_JUAN)
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
@@ -156,7 +156,7 @@ def DibujarNombres(img, rvecs_img, tvecs_img, newcameramtx, dist):
 if __name__ == '__main__':
 
     mtx, dist = calibrar()
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, ESCALA_BORJA, 0, ESCALA_BORJA)
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, ESCALA_JUAN, 0, ESCALA_JUAN)
 
     objpoints = np.zeros((9*6,3),np.float32)
     objpoints[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
@@ -164,7 +164,7 @@ if __name__ == '__main__':
 
     tiempos = []
 
-    cap = cv2.VideoCapture('VideoMovilBorja.mp4')
+    cap = cv2.VideoCapture('VideoPrueba.mp4')
     inicio = time.time()
     
     while(cap.isOpened()):
@@ -172,7 +172,7 @@ if __name__ == '__main__':
         ret, img = cap.read()
         
         if ret:
-            img = cv2.resize(img, ESCALA_BORJA)            
+            img = cv2.resize(img, ESCALA_JUAN)            
             img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             inicio_chessboard = time.time()
@@ -211,7 +211,7 @@ if __name__ == '__main__':
     cap.release()
 
 
-    with open("VideoPruebaBorja.csv", 'w') as f:
+    with open("VideoPruebaV2.csv", 'w') as f:
 
         csv.excel.delimiter=";"
         csv_writer = csv.writer(f, dialect=csv.excel)
