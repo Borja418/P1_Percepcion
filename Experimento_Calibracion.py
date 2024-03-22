3365628cfa22eb3561f718b092ea46e3c36f8587
Borja418 <borjapeiroten@gmail.com>
2024-03-22 15:38:47 +0100

    Comentar Experimento

--- Experimento_Calibracion.py
+++ Experimento_Calibracion.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Importar librerías
+
 import cv2
 import numpy as np
 import os
 
+# Variables globales
 
 LONGITUD = 3.6
 ESCALA_BORJA = (1280,720)
@@ -22,43 +25,53 @@ OBJPOINTS = OBJPOINTS*LONGITUD
 
 CRITERIA = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
+# Función de calibración de cámara empleando n imágenes
+
 def Calibrar(num_img):
     
-    
+    # Definición de variables
     count = 0
     objpoints_array = []
     imgpoints_array = []
 
-    imgs_name = [f for f in os.listdir(URI) if f.endswith('.jpg')]
+    imgs_name = [f for f in os.listdir(URI) if f.endswith('.jpg')]  # Obtener nombres de archivos dentro de la carpeta
 
     for img_name in imgs_name:
         
-        img = cv2.imread(URI+"/"+img_name)
-        # Comprobamos que la imagen se ha podido leer
-        if img is None:
+        img = cv2.imread(URI+"/"+img_name)  # Cargar imagen
+        
+        if img is None: # Comprobar que la imagen se ha podido leer
             print('Error al cargar la imagen')
             quit()
 
         img = cv2.resize(img, ESCALA)
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        ret, corners = cv2.findChessboardCorners(img_gray, (9,6), None)
+        ret, corners = cv2.findChessboardCorners(img_gray, (9,6), None)     # Encontrar esquinas del patrón de calibración
         
-        if ret:
-            count += 1
+        if ret:     # Si se ha podido encontrar
+                        
+            count += 1          # Añadir información neceria del punto para la calibración
             objpoints_array.append(OBJPOINTS)
 
             corners2 = cv2.cornerSubPix(img_gray, corners, (11,11), (-1,-1), CRITERIA)
             imgpoints_array.append(corners2)
         
-        if count == num_img:
+        if count == num_img:    # Si se alcanzan el número de imágenes establecidos para la calibración
             break
             
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints_array, imgpoints_array, img_gray.shape[::-1], None, None)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints_array, imgpoints_array, img_gray.shape[::-1], None, None)  # Calibrar la cámara
     
-    return mtx, dist
+    return mtx, dist    # Devolver parámetros intrínsecos
+
+
+    
+    # Calcular RMS del error de reproyección
 
 def Comprobar_Error(mtx, dist):
+    
+    # Definir variables necesarias
+
     mean_error = 0
     count_imgs = 0
     imgs_name = [f for f in os.listdir(URI) if f.endswith('.jpg')]
@@ -66,40 +79,43 @@ def Comprobar_Error(mtx, dist):
     objpoints_array = []
     imgpoints_array = []
 
+
     for img_name in imgs_name:
         
-        img = cv2.imread(URI+"/"+img_name)
+        img = cv2.imread(URI+"/"+img_name)  # Obtener imagen
         
-        if img is None:
+        if img is None:     # Comprobar que se ha cargado correctamente
             print('Error al cargar la imagen')
             quit()
 
         img = cv2.resize(img, ESCALA)
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        ret, corners = cv2.findChessboardCorners(img_gray, (9,6), None)
-        print(img_name, ret)
-        if ret:
+        ret, corners = cv2.findChessboardCorners(img_gray, (9,6), None) # Encontrar esquinas del patrón de calibración
+        
+        if ret:     # Si se ha encontrado el patrón
             
             count_imgs += 1
             corners2 = cv2.cornerSubPix(img_gray, corners, (11,11), (-1,-1), CRITERIA)
             
-            ret, rvecs, tvecs = cv2.solvePnP(OBJPOINTS, corners2, mtx, dist, True, cv2.SOLVEPNP_ITERATIVE)
+            ret, rvecs, tvecs = cv2.solvePnP(OBJPOINTS, corners2, mtx, dist, True, cv2.SOLVEPNP_ITERATIVE)  # Obtener parámetros extrínsecos
             
-            imgpoints2, _ = cv2.projectPoints(OBJPOINTS, rvecs, tvecs, mtx, dist)
-            error = cv2.norm(corners2, imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+            imgpoints2, _ = cv2.projectPoints(OBJPOINTS, rvecs, tvecs, mtx, dist)   # Proyectar puntos conociendo todos los parámetros de la cámara
+            
+            error = cv2.norm(corners2, imgpoints2, cv2.NORM_L2)/len(imgpoints2)     # Obtener error entre las proyecciones y esquinas del patrón encontrados.
             mean_error += error
     
 
-    mean_error = mean_error/count_imgs
+    mean_error = mean_error/count_imgs  # Calcular error medio
     
     return mean_error
 
 def main():
+    
     imgs_name = [f for f in os.listdir(URI) if f.endswith('.jpg')]
 
 
-    for num_img_calib in range(1,len(imgs_name)+1):
+    for num_img_calib in range(1,len(imgs_name)+1): # Cada iteración se añade una imagen más a la calibración
         
         mtx, dist = Calibrar(num_img_calib)
         
